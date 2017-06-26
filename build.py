9ee6babac8ea8832ba4be8b690f2f7b06b0560e2
pramodbhalerao <pramodbhalerao_01@yahoo.com>
2017-06-26 15:01:28 +0000

    Done

--- build.py
+++ build.py
@@ -1,14 +1,16 @@
 import operator
 
+d = {1: 2, 3: 4, 4: 3, 2: 1, 0: 0}
 
 def solution_asc(dic):
-    '''
-    Enter your code here
-    '''
+    dc_sort = sorted(dic.items(),key = operator.itemgetter(0),reverse = False)
+    return dc_sort
+
+print solution_asc(d)
 
 
 def solution_desc(dic):
-    '''
-    Enter your code here
-    '''
+    dc_sort = sorted(dic.items(),key = operator.itemgetter(0),reverse = True)
+    return dc_sort
 
+print solution_desc(d)
